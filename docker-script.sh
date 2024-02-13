acfa2894c3f6964b256231530cd1294df214e9fe
codingepaduli <padulodeveloper@protonmail.com>
2024-02-13 21:03:26 +0100

    Minor Fix - fixed script against shellcheck linter

--- docker-script.sh
+++ docker-script.sh
@@ -6,13 +6,13 @@
 currentDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Get the Mozilla profile folder from the file profiles.ini
-firefoxProfileFolder=`crudini --get "$HOME/.mozilla/firefox/profiles.ini" Profile0 Path`
+firefoxProfileFolder=$(crudini --get "$HOME/.mozilla/firefox/profiles.ini" Profile0 Path)
 
 # Mozilla Firefox bookmarks backup folder
 firefoxBackupFolder="$HOME/.mozilla/firefox/$firefoxProfileFolder/bookmarkbackups"
 
 # Set the last backup
-lastUpdatedFile=`ls -t "$firefoxBackupFolder" | head -n1`
+lastUpdatedFile=$(ls -t "$firefoxBackupFolder" | head -n1)
 
 # Set the output folder
 outputFolder="$currentDir/output"
@@ -20,5 +20,5 @@ outputFolder="$currentDir/output"
 # Create the output folder if not exists
 mkdir -p "output"
 
-docker container run --rm -it -u $(id -u ${USER}):$(id -g ${USER}) --name MozLz4-decompress -v "$PWD":/usr/src/myapp -v "$PWD/.local":"/.local" -v "$firefoxBackupFolder":/usr/src/bookmarksFolder -w /usr/src/myapp -e PYTHONDONTWRITEBYTECODE=1 python:3.10-slim /bin/bash -l -c "pip install --no-cache-dir lz4 && python /usr/src/myapp/mozLz4-decompress/mozlz4.py -d /usr/src/bookmarksFolder/$lastUpdatedFile /usr/src/myapp/output/bookmarks.json"
-docker container run --rm -it -u $(id -u ${USER}):$(id -g ${USER}) --name exportToMarkdown  -v "$PWD":/usr/src/myapp -v "$PWD/.local":"/.local" -v "$firefoxBackupFolder":/usr/src/bookmarksFolder -w /usr/src/myapp -e PYTHONDONTWRITEBYTECODE=1 python:3.10-slim /bin/bash -l -c "python3 exportToMarkdown.py /usr/src/myapp/output/bookmarks.json" > "$outputFolder/bookmarks.md"
+docker container run --rm -it -u $(id -u ${USER}):$(id -g ${USER}) --name MozLz4-decompress -v "$PWD":/usr/src/myapp -v "$PWD/.local":"/.local" -v "$firefoxBackupFolder":/usr/src/bookmarksFolder -w /usr/src/myapp -e PYTHONDONTWRITEBYTECODE=1 python:3.11.4-bookworm /bin/bash -l -c "pip install --no-cache-dir lz4 && python /usr/src/myapp/mozLz4-decompress/mozlz4.py -d /usr/src/bookmarksFolder/$lastUpdatedFile /usr/src/myapp/output/bookmarks.json"
+docker container run --rm -it -u $(id -u ${USER}):$(id -g ${USER}) --name exportToMarkdown  -v "$PWD":/usr/src/myapp -v "$PWD/.local":"/.local" -v "$firefoxBackupFolder":/usr/src/bookmarksFolder -w /usr/src/myapp -e PYTHONDONTWRITEBYTECODE=1 python:3.11.4-bookworm /bin/bash -l -c "python3 exportToMarkdown.py /usr/src/myapp/output/bookmarks.json" > "$outputFolder/bookmarks.md"
--- script.sh
+++ script.sh
@@ -6,13 +6,13 @@
 currentDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Get the Mozilla profile folder from the file profiles.ini
-firefoxProfileFolder=`crudini --get "$HOME/.mozilla/firefox/profiles.ini" Profile0 Path`
+firefoxProfileFolder=$(crudini --get "$HOME/.mozilla/firefox/profiles.ini" Profile0 Path)
 
 # Mozilla Firefox bookmarks backup folder
 firefoxBackupFolder="$HOME/.mozilla/firefox/$firefoxProfileFolder/bookmarkbackups"
 
 # Set the last backup
-lastUpdatedFile=`ls -t "$firefoxBackupFolder" | head -n1`
+lastUpdatedFile=$(ls -t "$firefoxBackupFolder" | head -n1)
 
 # Set the output folder
 outputFolder="$currentDir/output"
